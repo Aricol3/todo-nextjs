bc039a5b0254be3032a03f8f62483f68b894a3fd
adimetro <valeriu-adrian.surdu@metro.digital>
2024-04-23 14:53:59 +0300

    retry offline call

--- services/api.js
+++ services/api.js
@@ -1,69 +1,84 @@
 const baseUrl = "http://localhost:3001";
+const maxRetryAttempts = 20;
+const retryDelay = 3000;
+
+const retry = async (fn, retriesLeft = maxRetryAttempts) => {
+    try {
+        return await fn();
+    } catch (error) {
+        if (retriesLeft === 0) {
+            throw new Error('Max retries exceeded');
+        }
+        console.log("NU")
+        await new Promise(resolve => setTimeout(resolve, retryDelay));
+        return await retry(fn, retriesLeft - 1);
+    }
+};
 
 export const getAllTodos = async (queryParams) => {
     try {
-        // Construct the query string from the provided queryParams object
         const queryString = new URLSearchParams(queryParams).toString();
-
-        // Append the query string to the base URL
         const url = `${baseUrl}/tasks${queryString ? '?' + queryString : ''}`;
-
-        // Make an HTTP GET request to the constructed URL
-        const res = await fetch(url, { cache: "no-store" });
-
-        // Check if the response is not successful
+        const res = await fetch(url, {cache: "no-store"});
         if (!res.ok) {
             throw new Error('Failed to fetch todos');
         }
 
-        // Parse the JSON response body and return it
         return res.json();
     } catch (error) {
-        // If any errors occur during the process, throw them
         throw error;
     }
 };
 
 
 export const addTodo = async (newTodo) => {
-    try {
-        const res = await fetch(`${baseUrl}/tasks/`, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(newTodo),
-        });
-        if (!res.ok) {
-            throw new Error('Failed to add todo');
+    const addTodoAsync = async () => {
+        try {
+            const res = await fetch(`${baseUrl}/tasks/`, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(newTodo),
+            });
+            if (!res.ok) {
+                throw new Error('Failed to add todo');
+            }
+            return await res.json();
+        } catch (error) {
+            throw error;
         }
-        return await res.json();
-    } catch (error) {
-        throw error;
     }
+    return await retry(addTodoAsync)
 };
 
 export const editTodo = async (todo) => {
-    try {
-        const res = await fetch(`${baseUrl}/tasks/edit`, {
-            method: "PUT",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(todo),
-        });
-        if (!res.ok) {
-            throw new Error('Failed to edit todo');
+    const editTodoAsync = async () => {
+        try {
+            const res = await fetch(`${baseUrl}/tasks/edit`, {
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(todo),
+            });
+            if (!res.ok) {
+                throw new Error('Failed to edit todo');
+            }
+            return await res.json();
+        } catch (error) {
+            throw error;
         }
-        return await res.json();
-    } catch (error) {
-        throw error;
     }
+    return await retry(editTodoAsync)
 };
 
 export const deleteTodo = async (taskId) => {
-    try {
-        const res = await fetch(`${baseUrl}/tasks/${taskId}`, {method: "DELETE"});
-        if (!res.ok) {
-            throw new Error('Failed to delete todo');
+    const deleteTodoAsync = async () => {
+        try {
+            const res = await fetch(`${baseUrl}/tasks/${taskId}`, {method: "DELETE"});
+            if (!res.ok) {
+                throw new Error('Failed to delete todo');
+            }
+        } catch (error) {
+            throw error;
         }
-    } catch (error) {
-        throw error;
     }
+    return await retry(deleteTodoAsync)
 };
